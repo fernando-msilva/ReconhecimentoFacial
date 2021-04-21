e2fad971bce62a5d3f0df88a28708a4e338a845c
fernando-msilva <fernandogbi96@gmail.com>
2021-04-21 04:32:58 -0300

    Adicionando configuração para build de produção e desenvolvimento

--- api/src/spyface/Reconhecer.py
+++ api/src/spyface/Reconhecer.py
@@ -4,41 +4,31 @@ import numpy as np
 from statistics import mean
 
 recognizer = cv2.face.EigenFaceRecognizer_create(num_components=36)
-detector_faces_01 = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  # Treinamento da Detecção de faces
+detector_faces_01 = cv2.CascadeClassifier("/opt/ml/haarcascade_frontalface_default.xml")  # Treinamento da Detecção de faces
 
 def reconhecedor_eigen(id, documento_id):
-    recognizer.read(f'EigenClassifier.yml')
+    recognizer.read('/opt/ml/EigenClassifier.yml')
     trust_list = []
     width, height = 150, 150
     font = cv2.FONT_HERSHEY_COMPLEX
     n = 0
 
-    #paths = [os.path.join(path3, f) for f in os.listdir(path3)]
-
-    nome, imagens = recuperar(id,"teste")
-
-    for imagem_nome, imagem in imagens:
-        #imagem = cv2.imread(pathImage)  # abre a imagem
-        #nparr = np.fromstring(imagem_bytes, np.uint8)
-        #imagem = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
-        imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)  # Converte a imagem em escala de cinza
-        faces_detectadas = detector_faces_01.detectMultiScale(imagem_cinza, scaleFactor=1.2, minSize=(150, 150)) #Detecta faces dentro da foto
-        for (x, y, w, h) in faces_detectadas:
-            try:
-                imagem_face = cv2.resize(imagem_cinza[y:y + h, x:x + w], (width, height))
-                id, trust = recognizer.predict(imagem_face)
-                if trust < 8000.0:
-                    n = n + 1
-                    cv2.rectangle(imagem_cinza, (x, y), (x + w, y + h), (255, 255, 255, 2))
-                    cv2.putText(imagem_cinza, str(f"ID:{id}"), (x, y + (h + 65)), font, 3, (255, 255, 255), 2)
-                    cv2.putText(imagem_cinza, str(f"trust:{trust}"), (x, y + (h - 60)), font, 2, (255, 255, 255), 2)
-                    cv2.imshow("Face", imagem_cinza)
-                    cv2.waitKey(80)
-                    trust_list.append([imagem_nome, trust])
-                else:
-                    continue
-            except Exception as e:
-                print(e)
+    #imagem = cv2.imread(pathImage)  # abre a imagem
+    #nparr = np.fromstring(imagem_bytes, np.uint8)
+    #imagem = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
+    imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)  # Converte a imagem em escala de cinza
+    faces_detectadas = detector_faces_01.detectMultiScale(imagem_cinza, scaleFactor=1.2, minSize=(150, 150)) #Detecta faces dentro da foto
+    for (x, y, w, h) in faces_detectadas:
+        try:
+            imagem_face = cv2.resize(imagem_cinza[y:y + h, x:x + w], (width, height))
+            id, trust = recognizer.predict(imagem_face)
+            if trust < 8000.0:
+                n = n + 1
+                trust_list.append([imagem_nome, trust])
+            else:
+                continue
+        except Exception as e:
+            print(e)
     
     return trust_list
 
