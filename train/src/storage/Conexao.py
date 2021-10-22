f9821358ad7911eadc1df09d8bf13e972471410a
fernando-msilva <fernandogbi96@gmail.com>
2021-10-22 01:11:59 -0300

    correção no chart do mongodb e adicionando variável com o host do mongo

--- train/src/storage/Conexao.py
+++ train/src/storage/Conexao.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
+import os
 
 def conexao():
     try:
-        client = MongoClient("teste-mongodb-mongochart", 27017)
+        client = MongoClient(os.environ["MONGO_HOST"], 27017)
         db = client.spyface
         return db
     except Exception as e:
